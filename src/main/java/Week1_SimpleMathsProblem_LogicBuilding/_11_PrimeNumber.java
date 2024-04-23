b4053e0076eb615855867074462ed24b23aa2938
Kaustav Bag <kaustav.sdet@gmail.com>
2024-04-23 23:47:38 +0530

    Latest changes

--- src/main/java/Week1_SimpleMathsProblem_LogicBuilding/_11_PrimeNumber.java
+++ src/main/java/Week1_SimpleMathsProblem_LogicBuilding/_11_PrimeNumber.java
@@ -1,2 +1,27 @@
-package Week1_SimpleMathsProblem_LogicBuilding;public class _11_PrimeNumber {
+package Week1_SimpleMathsProblem_LogicBuilding;
+
+
+public class _11_PrimeNumber {
+
+    static boolean primeNumber(int number){
+        if (number <2) return false;
+
+        int n=2;
+
+        while(n*n<=number){
+            if(number%n==0){
+                return false;
+            }
+            n++;
+        }
+        return n*n>number;
+    }
+
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(primeNumber(i)){
+                System.out.println(i);
+            }
+        }
+    }
 }
