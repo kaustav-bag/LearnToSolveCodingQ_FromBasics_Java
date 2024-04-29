d909ef6df5c98bb209ed0bce6eac1c17571528d9
Kaustav Bag <kaustav.sdet@gmail.com>
2024-04-30 00:33:22 +0530

    Added

--- src/main/java/Week2/Factorial_Recursion.java
+++ src/main/java/Week2/Factorial_Recursion.java
@@ -4,13 +4,13 @@ public class Factorial_Recursion {
 
     static int factorial(int num){
 
-        if (num<1) return 1;
+        if (num==1) return 1;
 
-        int sum= factorial(num*(num-1));
-        return sum;
+        return num*factorial(num-1);
+
     }
 
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(factorial(7));
     }
 }
