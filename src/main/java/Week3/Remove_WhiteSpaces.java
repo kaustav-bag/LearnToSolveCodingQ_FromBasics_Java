e5a76c557ee6e838975b60c31f71c19ce9e2d18e
Kaustav Bag <kaustav.sdet@gmail.com>
2024-06-23 12:47:20 +0530

    added sunday

--- src/main/java/Week3/Remove_WhiteSpaces.java
+++ src/main/java/Week3/Remove_WhiteSpaces.java
@@ -5,12 +5,13 @@ public class Remove_WhiteSpaces {
     // Remove WhiteSpaces from a String
 
     public static void main(String[] args) {
-        System.out.println(removeWhiteSpace("nkjh ,jnjk jhghj  jkbjkh     j     kljl"));
+        String str="My Name is    KB";
+        System.out.println(removeWhiteSpace(str));
     }
 
 
     static String removeWhiteSpace(String str){
-        String newStr=str.replace("\\s","");
-        return str;
+        String newStr=str.replaceAll("\\s","");
+        return newStr;
     }
 }
